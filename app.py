f9e7e03180dbe6e880abdbc843af87862aeaebfc
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-07 12:10:28 +1200

    added show_all_recipes()

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ import sqlite3
 
 #   constants
 DBNAME = "RecipeBook.db"
+INTWIDTH = 5
+STRWIDTH = 10
 
 #   functions
+
+
+def show_all_recipes():
+    #   shows instructions and ingredients for a recipe
+    db = sqlite3.connect(DBNAME)
+    cursor = db.cursor()
+    sql = """SELECT * FROM Recipes"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return results
 
 
 def show_recipe(recipe):
@@ -36,7 +48,9 @@ def show_ingredient(food):
     results = cursor.fetchall()
     return results
 
+
 #   menu
+print(show_all_recipes())
 print(show_recipe(1))
 print(show_meal(5))
 print(show_ingredient(2))
