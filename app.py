53aabc72a676cfda55ac17b0095f8bea2dbe855e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-09 14:43:48 +1200

    set up app.py

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import sqlite3
 app = Flask(__name__)
 DB = "RecipeBook.db"
 
-@app.route("/")
+@app.route('/')
+def home():
+    return 'Welcome'
+
+if __name__ == "__main__":
+    app.run(debug=True)
