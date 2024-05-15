395cd82d19aa0942043d04f8e51991fbd4d0b26c
yajilin <yajilingame@gmail.com>
2024-05-15 19:05:35 +1200

    addded more recipes/data

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def allrecipes():
     db.close()
     return render_template('home.html', results = results)
 
-@app.route('/recipe/<recipe>')
-    def recipe(name):
+#   @app.route('/recipe/<recipe>')
+    #   def recipe(name):
 
 
 if __name__ == "__main__":
