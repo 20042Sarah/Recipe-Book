baf1238b4e5b8d7a2cc55dd9506ba03e73311807
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-20 12:21:14 +1200

    added page_not_found(error) & other error messages

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 DB = "RecipeBook.db"
 
 
+#   home page
 @app.route('/')
 def allrecipes():
     db = sqlite3.connect(DB)
@@ -17,24 +18,37 @@ def allrecipes():
     return render_template('home.html', results=results)
 
 
+#   recipe page
 @app.route('/recipe/<name>')
 def displayrecipe(name):
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    sql = """SELECT * FROM Recipes WHERE Recipes.Name = '%s';""" % name
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    recipe = results[0][0]
-    sql = """SELECT Food.Name AS Ingredient, Ingredients.Quantity
-        FROM Ingredients LEFT JOIN Food ON Ingredients.Food = Food.FoodID
-        WHERE Recipe = %s;""" % recipe
-    cursor.execute(sql)
-    r1 = cursor.fetchall()
-    sql = """SELECT Instructions.Step, Instructions.Instruction FROM
-        Instructions WHERE Recipe = %s;""" % recipe
-    cursor.execute(sql)
-    r2 = cursor.fetchall()
+    try:
+        sql = """SELECT * FROM Recipes WHERE Recipes.Name = '%s';""" % name
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        recipe = results[0][0]
+        sql = """SELECT Food.Name AS Ingredient, Ingredients.Quantity
+            FROM Ingredients LEFT JOIN Food ON Ingredients.Food = Food.FoodID
+            WHERE Recipe = %s;""" % recipe
+        cursor.execute(sql)
+        r1 = cursor.fetchall()
+        sql = """SELECT Instructions.Step, Instructions.Instruction FROM
+            Instructions WHERE Recipe = %s;""" % recipe
+        cursor.execute(sql)
+        r2 = cursor.fetchall()
+    except IndexError:
+        #   if the recipe name is not in the database and error message is given
+        error = 'Page not found. Please check the recipe name.'
+        return render_template('error.html', error=error)
     return render_template('recipe.html', recipe=name, ingred=r1, instr=r2)
+
+
+#   page not found error page
+@app.errorhandler(404)
+def page_not_found(error):
+    error = 'Page not found. Please check that the address is spelt correctly.'
+    return render_template('error.html', error=error)
 
 
 if __name__ == "__main__":
